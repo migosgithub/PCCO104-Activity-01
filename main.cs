a26bb76ca1d97de2e5ff00031ddd0217fc66fac9
migosgithub <158656983+migosgithub@users.noreply.github.com>
2024-02-03 03:21:41 +0000

    Added "Teacher" and "Subject"

--- main.cs
+++ main.cs
@@ -6,7 +6,9 @@ class Program {
     string firstName = "Cyrus";
     string lastName = "Salvador";
     int age = 19;
+    string subject = "Intermediate Programming";
+    string teacher = "Sir Ralph Lee";
     Console.WriteLine($"Hello World {firstName} {lastName} Age: {age}");
-    Console.WriteLine("Hello World " + firstName + " " + lastName + " " + age);
+    Console.WriteLine("Hello World " + firstName + " " + lastName + " " + age + " Teacher: " + teacher + " - " + subject);
   }
 }
